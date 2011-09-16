a2640d62f61fd61b05cf1b3dcaa3fa1a3966b9a3
Erdgeist <unknown>
2011-09-16 15:54:03 +0200

    Add some disenchant_loot_templates for ICC

---
+++ World/disenchant_loot_template.IceCrownCitadel.sql
@@ -0,0 +1,12 @@
+/*
+SQLyog Community Edition- MySQL GUI v8.14 
+MySQL - 5.0.51a-24+lenny4 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `disenchant_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('50444','34057','50','1','0','1','1');
+insert into `disenchant_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('50452','34057','50','1','0','1','1');
+insert into `disenchant_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('50447','34057','50','1','0','1','1');
+insert into `disenchant_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('50453','34057','50','1','0','1','1');
+insert into `disenchant_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) values('50449','34057','50','1','0','1','1');
