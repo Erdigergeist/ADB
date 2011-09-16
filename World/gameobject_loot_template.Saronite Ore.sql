1193f9086b7ca91f31133a81efef0add7e29a79b
Erdgeist <unknown>
2011-09-16 15:43:50 +0200

    Update some min/maxcount for Saronite Ore

---
+++ World/gameobject_loot_template.Saronite Ore.sql
@@ -0,0 +1,3 @@
+UPDATE gameobject_loot_template SET `mincountOrRef` = '2', `maxcount` = '4' WHERE `entry` = '24155' AND `item` = '36912';
+UPDATE gameobject_loot_template SET `mincountOrRef` = '5', `maxcount` = '7' WHERE `entry` = '24156' AND `item` = '36912';
+UPDATE gameobject_loot_template SET `mincountOrRef` = '11', `maxcount` = '16' WHERE `entry` = '27244' AND `item` = '36912';
