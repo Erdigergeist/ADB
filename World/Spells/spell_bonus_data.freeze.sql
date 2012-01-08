57c5d5e10419820fa74b8429c41169d24e2fa0e3
AliveShiro <alu@senzaii.net>
2012-01-08 17:09:48 +0100

    More enchantments that should not scale with spell power

---
+++ World/Spells/spell_bonus_data.freeze.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_bonus_data WHERE entry = '18798';
+INSERT INTO spell_bonus_data VALUES ('18798','0','0','0','0','Freeze proc');
---
+++ World/Spells/spell_bonus_data.lightning_strike.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_bonus_data WHERE entry = '16614';
+INSERT INTO spell_bonus_data VALUES ('16614','0','0','0','0','Lightning Strike proc');
