745b478177809a87e7dac8a96bb0aca6151540eb
AliveShiro <alu@senzaii.net>
2012-01-10 01:19:43 +0100

    Fix Quest The Valiant's Challenge

---
+++ World/Continents/Northrend/Icecrown/creature_template.squire_danny_and_Argent_Champion.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `ScriptName` = 'npc_squire_danny' WHERE `entry` = '33518';
+UPDATE `creature_template` SET `ScriptName` = 'npc_argent_champion' WHERE `entry` = '33707';
