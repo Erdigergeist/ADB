d8f3b213cda78e7d20ad1a95de90f14c96684351
Shiro <alu@senzaii.net>
2012-02-11 22:49:27 +0100

    Hochzeit Items und gameobjekte

---
+++ World/Customs/gameobject.Hochzeit.sql
@@ -0,0 +1,250 @@
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359762, 182628, 0, 1, 2, -8561.29, 829.386, 106.532, 0.675038, 0, 0, 0.331147, 0.943579, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359708, 182628, 0, 1, 2, -8557.4, 832.494, 106.52, 0.667194, 0, 0, 0.327444, 0.944871, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359769, 182628, 0, 1, 2, -8553.71, 835.482, 106.52, 0.702531, 0, 0, 0.344086, 0.938938, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359774, 182628, 0, 1, 2, -8549.96, 838.388, 106.518, 0.671115, 0, 0, 0.329296, 0.944227, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359781, 182628, 0, 1, 2, -8546.3, 841.376, 106.519, 0.700947, 0, 0, 0.343343, 0.93921, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359784, 181150, 0, 1, 2, -8514.32, 860.796, 111.04, 3.77498, 0, 0, 0.95027, -0.311426, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359802, 182628, 0, 1, 2, -8542.6, 844.255, 106.519, 0.685239, 0, 0, 0.335956, 0.941878, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359818, 182628, 0, 1, 2, -8538.92, 847.239, 106.519, 0.673457, 0, 0, 0.330401, 0.943841, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359863, 182628, 0, 1, 2, -8535.25, 850.169, 106.52, 0.681311, 0, 0, 0.334105, 0.942536, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359867, 182628, 0, 1, 2, -8531.54, 853.091, 106.52, 0.689165, 0, 0, 0.337804, 0.941217, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359904, 182628, 0, 1, 2, -8555.42, 822.046, 106.533, 0.675032, 0, 0, 0.331144, 0.94358, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359906, 182628, 0, 1, 2, -8551.61, 825.068, 106.521, 0.69074, 0, 0, 0.338545, 0.94095, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359909, 182628, 0, 1, 2, -8547.89, 827.981, 106.52, 0.688384, 0, 0, 0.337436, 0.941348, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359911, 182628, 0, 1, 2, -8544.32, 830.863, 106.519, 0.676603, 0, 0, 0.331886, 0.94332, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359969, 182628, 0, 1, 2, -8540.44, 833.937, 106.519, 0.653043, 0, 0, 0.32075, 0.947164, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359959, 179673, 0, 1, 2, -8531.46, 857.612, 106.518, 1.42976, 0, 0, 0.655527, 0.755172, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359960, 179673, 0, 1, 2, -8521.42, 844.89, 106.534, 6.18534, 0, 0, 0.0489017, -0.998804, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359970, 179673, 0, 1, 2, -8511.97, 852.609, 109.806, 3.92734, 0, 0, 0.923813, -0.382843, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359971, 179673, 0, 1, 2, -8521.75, 865.007, 109.796, 3.76633, 0, 0, 0.951608, -0.307313, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359972, 182628, 0, 1, 2, -8536.77, 836.854, 106.519, 0.6766, 0, 0, 0.331884, 0.94332, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359973, 179673, 0, 1, 2, -8524.29, 863.279, 106.52, 1.48082, 0, 0, 0.674592, 0.73819, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359974, 182628, 0, 1, 2, -8533, 839.865, 106.52, 0.692308, 0, 0, 0.339282, 0.940685, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359975, 179673, 0, 1, 2, -8514.14, 850.451, 106.535, 6.08798, 0, 0, 0.09745, -0.99524, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359976, 182628, 0, 1, 2, -8529.2, 842.884, 106.52, 0.6766, 0, 0, 0.331884, 0.94332, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1359977, 182628, 0, 1, 2, -8525.74, 845.656, 106.519, 0.668746, 0, 0, 0.328177, 0.944616, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360052, 180425, 0, 1, 2, -8527.82, 857.401, 106.798, 3.95014, 0, 0, 0.919387, -0.393353, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360053, 180425, 0, 1, 2, -8520.59, 848.45, 106.798, 3.70274, 0, 0, 0.960897, -0.276907, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360183, 180339, 0, 1, 2, -8519.14, 860.627, 109.989, 3.7695, 0, 0, 0.95112, -0.308821, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360184, 180339, 0, 1, 2, -8515.52, 856.112, 109.989, 3.73809, 0, 0, 0.955853, -0.293846, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360185, 180340, 0, 1, 2, -8515.55, 861.216, 111.039, 1.39367, 0, 0, 0.641796, 0.766876, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360186, 180340, 0, 1, 2, -8514.18, 859.423, 111.039, 0.0585019, 0, 0, 0.0292468, 0.999572, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360187, 180415, 0, 1, 2, -8517.38, 853.467, 108.923, 3.97372, 0, 0, 0.914687, -0.404163, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360188, 180415, 0, 1, 2, -8518.28, 851.871, 108.263, 4.00906, 0, 0, 0.907403, -0.420262, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360189, 180415, 0, 1, 2, -8522.24, 859.401, 108.88, 3.66034, 0, 0, 0.96655, -0.256478, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360190, 180415, 0, 1, 2, -8523.86, 858.857, 108.257, 3.62107, 0, 0, 0.971399, -0.237451, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360241, 179673, 0, 1, 2, -8546.72, 810.104, 106.534, 4.49916, 0, 0, 0.778337, -0.627847, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360242, 179673, 0, 1, 2, -8532.47, 821.577, 106.531, 4.49366, 0, 0, 0.78006, -0.625704, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360243, 179673, 0, 1, 2, -8528.63, 824.515, 106.532, 0.0679374, 0, 0, 0.0339621, 0.999423, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360245, 179673, 0, 1, 2, -8539.57, 815.954, 106.531, 4.5392, 0, 0, 0.765611, -0.643303, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360246, 179673, 0, 1, 2, -8521.37, 830.269, 106.531, 6.13199, 0, 0, 0.0755264, -0.997144, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360247, 179673, 0, 1, 2, -8514.23, 835.909, 106.533, 6.16734, 0, 0, 0.0578908, -0.998323, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360248, 179673, 0, 1, 2, -8507.2, 841.487, 106.535, 0.06872, 0, 0, 0.0343532, 0.99941, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360293, 179673, 0, 1, 2, -8531.43, 872.113, 106.517, 1.45417, 0, 0, 0.664695, 0.747115, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360294, 179673, 0, 1, 2, -8538.52, 866.439, 106.517, 1.34029, 0, 0, 0.621099, 0.783732, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360295, 179673, 0, 1, 2, -8542.4, 863.392, 106.517, 3.07602, 0, 0, 0.999463, 0.0327812, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360304, 179673, 0, 1, 2, -8549.79, 857.755, 106.533, 3.12314, 0, 0, 0.999957, 0.00922674, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360305, 179673, 0, 1, 2, -8556.84, 852.245, 106.533, 3.39017, 0, 0, 0.992286, -0.123971, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360306, 179673, 0, 1, 2, -8563.94, 846.501, 106.531, 3.17812, 0, 0, 0.999833, -0.0182611, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360307, 179673, 0, 1, 2, -8571.04, 840.779, 106.535, 3.00533, 0, 0, 0.99768, 0.0680791, 300, 0, 1);
+
+INSERT INTO gameobject
+   (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`)
+VALUES
+   (1360639, 178649, 0, 1, 2, -8560.66, 823.918, 116.718, 0.668761, 0, 0, 0.328184, 0.944614, 300, 0, 1);
+   
